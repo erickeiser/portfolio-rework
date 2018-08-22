e1cb8df153f11486a6534814777e079c5304848e
EricKeiser <eric.keiser1@gmail.com>
2018-08-22 00:09:52 -0400

    first commit

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+
+  // skills button press
+  $('#skills-target').click(function(){
+   $('#skills').toggle('slow')
+   $('#skills').css('display','block')
+   $('#home').css('display','none')
+  })
+
+
+})
+
